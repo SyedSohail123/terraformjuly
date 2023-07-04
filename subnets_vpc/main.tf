25ab627f5f954ea08390238d79b0585eafecacdb
Sohail <sohail@123.com>
2023-07-04 18:55:28 +0530

    Added subnets

--- subnets_vpc/main.tf
+++ subnets_vpc/main.tf
@@ -9,7 +9,11 @@ resource "aws_subnet" "ntier" {
     count = length(var.ntier_vpc_info.subnet_names)
     cidr_block = cidrsubnet(var.ntier_vpc_info.vpc_cidr,8,count.index)
     availability_zone = "${var.region}${var.ntier_vpc_info.subnet_azs[count.index]}"
+    vpc_id = aws_vpc.ntier.id
     tags = {
       Name = var.ntier_vpc_info.subnet_names[count.index]
     }
+    depends_on = [ 
+        aws_vpc.ntier
+         ]
 }
